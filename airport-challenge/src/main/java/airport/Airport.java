f12d276fabf1f538995d285f051e180a77b33a98
Luigi <l-palermo@hotmail.it>
2019-10-10 21:46:08 +0100

    dev: Add condition full hangar

--- airport-challenge/src/main/java/airport/Airport.java
+++ airport-challenge/src/main/java/airport/Airport.java
@@ -1,18 +1,21 @@
 package airport;
 
+import airplane.Airplane;
+
 import java.util.ArrayList;
 
 public class Airport
 {
   ArrayList<Object> hangar = new ArrayList<Object>();
 
-  public Object airplaneLand(Object airplane)
+  public Object airplaneLand(Airplane airplane)
   {
+    if (hangar.size() == 5) { return "The hangar is full"; }
     hangar.add(airplane);
     return hangar;
   }
 
-  public Object airplaneTakeOff(Object airplane)
+  public Object airplaneTakeOff(Airplane airplane)
   {
     hangar.remove(airplane);
     return hangar;
--- airport-challenge/src/test/java/airport/AirportTest.java
+++ airport-challenge/src/test/java/airport/AirportTest.java
@@ -2,11 +2,14 @@ package airport;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Test;
 
 import airplane.Airplane;
+
+import static org.mockito.Mockito.*;
 
 public class AirportTest
 {
@@ -21,17 +24,26 @@ public class AirportTest
   public void returnsAnArrayWithTheLandedAirplane()
   {
     Airport airport = new Airport();
-    Airplane airplane = new Airplane();
-    airport.airplaneLand(airplane);
-    assertEquals(Arrays.asList(airplane), airport.hangar);
+    Airplane airplaneMock = mock(Airplane.class);
+    airport.airplaneLand(airplaneMock);
+    assertEquals(Arrays.asList(airplaneMock), airport.hangar);
   }
   @Test
   public void returnsAnArrayWithoutTheDepartedAirplane()
   {
     Airport airport = new Airport();
-    Airplane airplane = new Airplane();
-    airport.hangar.add(airplane);
-    airport.airplaneTakeOff(airplane);
+    Airplane airplaneMock = mock(Airplane.class);
+    airport.hangar.add(airplaneMock);
+    airport.airplaneTakeOff(airplaneMock);
     assertEquals(Arrays.asList(), airport.hangar);
+  }
+  @Test
+  public void AirplaneCannotLandIfHagarFull()
+  {
+    Airport airport = new Airport();
+    Airplane airplaneMock = mock(Airplane.class);
+    Object[] hangarFull = new Object[5];
+    airport.hangar = new ArrayList<>(Arrays.asList(hangarFull));
+    assertEquals("The hangar is full", airport.airplaneLand(airplaneMock));
   }
 }
