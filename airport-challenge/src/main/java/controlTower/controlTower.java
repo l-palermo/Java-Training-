e913579288115a2abaf9f64f84d1439f042e58da
Luigi <l-palermo@hotmail.it>
2019-10-20 13:14:58 +0100

    README

--- airport-challenge/src/main/java/controlTower/controlTower.java
+++ airport-challenge/src/main/java/controlTower/controlTower.java
@@ -10,7 +10,7 @@ public class ControlTower
   // to land or take off - Refactor conditional to smaller private methods
   public boolean checkForLand(ArrayList<Object> hangar, Airplane airplane, String weather)
   {
-    if (checkHangarFull(hangar) || hangar.contains(airplane) || 
+    if (checkHangarFull(hangar) || checkHangarWithAirplane(hangar, airplane) || 
     airplane.atAirport() == true || weather == "stormy")
     {
      return true;
@@ -30,6 +30,11 @@ public class ControlTower
   private boolean checkHangarFull(ArrayList<Object> hangar)
   {
     if (hangar.size() >= 5) { return true; }
+    return false;
+  }
+  private boolean checkHangarWithAirplane(ArrayList<Object> hangar, Airplane airplane)
+  {
+    if (hangar.contains(airplane)) { return true; }
     return false;
   }
 }
