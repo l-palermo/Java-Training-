465ad9de26609e13770806c753d1e46efac0c561
Luigi <l-palermo@hotmail.it>
2019-10-20 13:27:21 +0100

    dev: Refactor control tower

--- airport-challenge/src/main/java/controlTower/controlTower.java
+++ airport-challenge/src/main/java/controlTower/controlTower.java
@@ -6,27 +6,25 @@ import airplane.Airplane;
 
 public class ControlTower
 {
-  // insert control tower message to display reason of airplane not allowed 
-  // to land or take off - Refactor conditional to smaller private methods
   public boolean checkForLand(ArrayList<Object> hangar, Airplane airplane, String weather)
   {
     if (checkHangarFull(hangar) || checkHangarWithAirplane(hangar, airplane) || 
-    airplane.atAirport() == true || weather == "stormy")
+    checkIfAirplaneIsAtAirport(airplane) || checkWeather(weather))
     {
      return true;
     }
     return false;
   }
-
   public boolean checkForTakeOff(ArrayList<Object> hangar, Airplane airplane, String weather)
   { 
-    if (!hangar.contains(airplane) || airplane.atAirport() == false ||
-    weather == "stormy")
+    if (!checkHangarWithAirplane(hangar, airplane) || !checkIfAirplaneIsAtAirport(airplane) ||
+    checkWeather(weather))
     {
       return true;
     }
     return false;
   }
+
   private boolean checkHangarFull(ArrayList<Object> hangar)
   {
     if (hangar.size() >= 5) { return true; }
@@ -35,6 +33,16 @@ public class ControlTower
   private boolean checkHangarWithAirplane(ArrayList<Object> hangar, Airplane airplane)
   {
     if (hangar.contains(airplane)) { return true; }
+    return false;
+  }
+  private boolean checkIfAirplaneIsAtAirport(Airplane airplane)
+  {
+    if (airplane.atAirport()) { return true; }
+    return false;
+  }
+  private boolean checkWeather(String weather)
+  {
+    if (weather == "stormy") { return true; }
     return false;
   }
 }
