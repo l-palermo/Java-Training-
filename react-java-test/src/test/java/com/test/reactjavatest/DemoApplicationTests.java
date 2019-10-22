21f42a04262694dfb4687d105f9ae4bba6f42cf2
Luigi <l-palermo@hotmail.it>
2019-10-22 14:00:26 +0100

    dev: changed to springboot

---
+++ react-java-test/src/test/java/com/test/reactjavatest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.reactjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
